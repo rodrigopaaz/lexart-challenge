e1ba31705fdafbe24d0c64aa38398bca8d7762d2
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-04-14 00:57:30 -0300

    FEAT CONECT FRONT AND BACK

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,54 @@
-import './App.css';
-import editRequest from './apis';
+import { useState } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
-const img1 = 'data:image/png;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7=='
-const img2 = 'data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==' 
-return (
+  const [site, setSite] = useState("");
+  const [category, setCategory] = useState("");
+  const [products, setProducts] = useState([]);
+
+  const getAll = async (site, category) => {
+    /*   await Axios.get("http://localhost:3003/product");
+     */
+    const { data } = await Axios.post("http://localhost:3003/product", {
+      site: site,
+      category: category,
+    });
+    setProducts(JSON.parse(data));
+    console.log(data);
+    return data;
+  };
+
+  return (
     <div className="App">
-      <img src={img1} alt="test" />
+      <label htmlFor="site">
+        site
+        <input
+          type="text"
+          onChange={({ target: { value } }) => setSite(value)}
+        />
+      </label>
+      <label htmlFor="category">
+        category
+        <input
+          type="text"
+          onChange={({ target: { value } }) => setCategory(value)}
+        />
+      </label>
+      <button type="button" onClick={async () => await getAll(site, category)}>
+        ok
+      </button>
+      {
+        <div>
+          {products.map((product) => (
+            <div>
+              <p>{product.title}</p>
+              <p>{`R$${product.price}`}</p>
+              <img src={product.imageUrl} alt="productImage" />
+            </div>
+          ))}
+        </div>
+      }
     </div>
   );
 }
