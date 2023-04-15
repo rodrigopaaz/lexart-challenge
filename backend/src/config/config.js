a6d967613b9f4903eb782f3ebaebb97f98712d6f
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-04-15 00:56:41 -0300

    FIX: INVALID IMAGE URLS BUSCAPE

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,17 +1,17 @@
 // src/config/config.js
 
-require('dotenv').config();
+require("dotenv").config();
 
 const config = {
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
+  username: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PASSWORD || "password",
   host: process.env.MYSQL_HOST,
-  dialect: 'mysql',
+  dialect: "mysql",
 };
 
 module.exports = {
   development: {
     ...config,
-    database: 'lexart_database',
-  }
+    database: "lexart_database",
+  },
 };
--- backend/src/services/data/buscapeData.js
+++ backend/src/services/data/buscapeData.js
@@ -3,37 +3,58 @@ const { default: axios } = require("axios");
 const cheerio = require("cheerio");
 
 const buscapeProducts = async (category) => {
-  const { data } = await axios.get(`https://buscape.com.br/${category}`);
+  const siteUrl = `https://buscape.com.br`;
+  const { data } = await axios.get(`${siteUrl}/${category}`);
   const $ = cheerio.load(data);
   const allProducts = [];
+  const links = [];
   const { id } = await Category.findOne({ where: { name: category } });
-  $(".col-lg-9 .Hits_Wrapper__3q_7P a").each(async (e, i) => {
-    const product = {
-      title: $(i)
-        .find(
-          ".Text_Text__h_AF6.Text_MobileLabelXs__ER_cD.Text_DesktopLabelSAtLarge__baj_g.SearchCard_ProductCard_Name__ZaO5o"
-        )
-        .text(),
-      categoryId: id,
-      siteId: 2,
-      price: $(i)
-        .find(".Text_Text__h_AF6.Text_MobileHeadingS__Zxam2")
-        .text()
-        .slice(3)
-        .split("R$")[0]
-        .split(",00")[0],
-      imageUrl: $(i)
-        .find(
-          ".SearchCard_ProductCard_Body__2wM_H .SearchCard_ProductCard_Image__ffKkn span img"
-        )
-        .attr("src"),
-    };
-    allProducts.push(product);
+
+  $(".col-lg-9 div").each(async (e, i) => {
+    const getLink =
+      $(i).find(".SearchCard_ProductCard_Inner__7JhKb").attr("href") || "";
+
+    if (!getLink.includes("buscape")) {
+      const product = {
+        title: $(i).find("a h2").text(),
+        categoryId: id,
+        siteId: 2,
+        price: $(i)
+          .find(
+            "a .SearchCard_ProductCard_Description__fGXI3 .Text_Text__h_AF6.Text_MobileHeadingS__Zxam2"
+          )
+          .text()
+          .slice(3)
+          .split("R$")[0]
+          .split(",00")[0],
+        linkUrl: getLink,
+      };
+      allProducts.push(product);
+      return product;
+    }
   });
   const filteredProducts = allProducts.filter(
-    (e, i) => e.price && e.title && e.imageUrl
+    (e, i) => e.price && typeof e.title && e.title.length < 100
   );
-  return filteredProducts;
-};
 
+  const getValidImage = () => {
+    return filteredProducts.map(async ({ linkUrl }, i) => {
+      const url = "https://buscape.com.br";
+      const fileData = await axios.get(url + linkUrl); //
+      const $$ = cheerio.load(fileData.data);
+      const imageSrc = $$(".ProductPageBody_ContentBody__De_1M")
+        .find("div:nth-child(2) img")
+        .attr("src");
+      const newArray = {
+        ...filteredProducts[i],
+        imageUrl: imageSrc,
+      };
+      return newArray;
+    });
+  };
+
+  const images = await Promise.all(getValidImage());
+
+  return images;
+};
 module.exports = buscapeProducts;
