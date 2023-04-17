e631811ff3b88f6160ad1d3e03264e9982bd5709
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-04-17 12:46:23 -0300

    FIX CONFIG

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -6,12 +6,13 @@ const config = {
   username: process.env.MYSQL_USER || "root",
   password: process.env.MYSQL_PASSWORD || "password",
   host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT || 3306,
   dialect: "mysql",
 };
 
 module.exports = {
   development: {
     ...config,
-    database: "lexart_database",
+    database: process.env.MYSQL_DATABASE || "lexart_database",
   },
 };
