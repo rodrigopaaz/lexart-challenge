ba5182579f578f92304d2cf8b1cefe4ab3b756c7
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-04-21 18:40:54 -0300

    FEAT FIX LOCAL HOST

--- frontend/src/services/requests.js
+++ frontend/src/services/requests.js
@@ -1,10 +1,12 @@
 import Axios from "axios";
 
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost'
+
 
 const post = async (site, category, search) => {
   try {
-    const { data } = await Axios.post(`http://localhost:${PORT}/product`, {
+    const { data } = await Axios.post(`http://${HOST}:${PORT}/product`, {
       site,
       category,
       search,
